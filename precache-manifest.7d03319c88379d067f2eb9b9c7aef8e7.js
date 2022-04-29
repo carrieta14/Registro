9cf7ecc64911b039617a2da531550c8c9858a3d6
Cristian Arrieta <carrieta14@cuc.edu.co>
2022-04-29 01:07:08 -0500

    Updates

---
+++ precache-manifest.7d03319c88379d067f2eb9b9c7aef8e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee3d2730fbc37ac92e9",
+    "url": "/Registro/static/css/main.91384a0b.chunk.css"
+  },
+  {
+    "revision": "dee3d2730fbc37ac92e9",
+    "url": "/Registro/static/js/main.dee3d273.chunk.js"
+  },
+  {
+    "revision": "eb1a0f2b9d953db6e453",
+    "url": "/Registro/static/js/1.eb1a0f2b.chunk.js"
+  },
+  {
+    "revision": "e43c08e2812ab89b5934",
+    "url": "/Registro/static/js/runtime~main.e43c08e2.js"
+  },
+  {
+    "revision": "3f2461368c08332e75590b75a4f5f01c",
+    "url": "/Registro/index.html"
+  }
+];
